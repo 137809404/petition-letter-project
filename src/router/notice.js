627fc8b9963466ff17e9b78b6d169fd2f151d1b5
Hang <137809404@qq.com>
2020-12-03 12:29:11 +0800

    系统通知

---
+++ src/router/notice.js
@@ -0,0 +1,17 @@
+import Layout from '@/views/layout'
+const routerMap = {
+    path: '/',
+    component: Layout,
+    meta: { active: false },
+    hidden: false,
+    name:'消息通知',
+    children: [
+        {
+            path: '/systemNotice',
+            name: '系统通知',
+            component: () => import('@/views/notice/systemNotice/index'),
+            meta: { active: false }
+        }
+    ]
+}
+export default routerMap
