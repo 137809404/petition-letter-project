5ea5f70b398048b80188f76d249244b0d69a1cca
Hang <137809404@qq.com>
2020-12-04 16:40:02 +0800

    更改视频信访页面路由

--- src/views/task/petitionVideo/petitionList.vue
+++ src/views/task/petitionVideo/petitionList.vue
@@ -1,10 +1,19 @@
 <template>
-<div>待信访人列表</div>
+<div>待信访人列表
+<el-button @click="goview('视频录制')">前往视频录制</el-button>
+</div>
 </template>
 
 <script>
 export default {
-name: "petitionList"
+name: "petitionList",
+  methods:{
+    goview (name) {
+      this.$router.push({ name }).catch(err => {
+        err && console.log('刷新') // 待优化
+      })
+    }
+  }
 }
 </script>
 
